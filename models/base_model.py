16fa92221f1f20b3ba9d13c01ad2c71f99bdf174
oreste-abizera <oresteabizera11@gmail.com>
2024-09-07 16:49:11 +0200

    feat: added FileStorage class for object serialization

--- models/base_model.py
+++ models/base_model.py
@@ -1,14 +1,27 @@
 import uuid
 from datetime import datetime
+import models
 
 class BaseModel:
     """Defines all common attributes/methods for other classes."""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initializes the BaseModel with unique ID and creation time."""
+        
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        
         self.id = str(uuid.uuid4())  # unique id converted to string
         self.created_at = datetime.now()  # current datetime for creation
         self.updated_at = datetime.now()  # setting updated_at to the same time as created_at
+        
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, tform))
+                elif key != '__class__':
+                    setattr(self, key, value)
+        else:
+            models.storage.new(self)
 
     def __str__(self):
         """String representation of the BaseModel instance."""
@@ -17,6 +30,7 @@ class BaseModel:
     def save(self):
         """Updates the updated_at attribute with the current datetime."""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values of the instance."""
